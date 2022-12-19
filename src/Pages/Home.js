99dc224555241fa4b6fcf6fecfdd2281cd441cfb
jimoh yusuf <expensive7832@gmail.com>
2022-12-19 17:38:25 +0100

    milestone2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,8 +7,11 @@ import { Navbar,
     NavItem,
     NavLink
  } from 'reactstrap'
-
- import Slider from 'react-slick'
+import { AiFillHtml5 } from "react-icons/ai"
+import { FaCss3Alt } from "react-icons/fa"
+import { TbBrandJavascript } from "react-icons/tb"
+import { BsBootstrap } from "react-icons/bs"
+import { GiFireBowl} from "react-icons/gi"
 
 function Home() {
 
@@ -24,6 +27,14 @@ function Home() {
   speed: 1000,
   autoPlay: true
  }
+
+
+ const info = [
+  {icon: <GiFireBowl size={28} color={"orange"}/>, text: "i am exceptional"},
+  {icon: <GiFireBowl size={28} color={"orange"}/>, text: "i am a javascript full stack developer"},
+  {icon: <GiFireBowl size={28} color={"orange"}/>, text: "please contact me"},
+
+ ]
 
   return ( 
     <div> 
@@ -46,12 +57,89 @@ function Home() {
         </Collapse>
       </Navbar>
 
-      <div className="banner">
-        <Slider {...settings}>
-          <img src="./../../display.jpeg" alt="" />
-          <img src="./../../login.jpg" alt="" />
-          
-        </Slider>
+      <div className="banner position-relative">
+        <p className="position-absolute top-50 start-50 text-white translate-middle">
+          <h2 className='fw-bold h1'>Esq. Yusuf</h2>
+          <p>welcome to my portfolio</p>
+          <div className='d-flex justify-content-between'>
+            <button className='btn btn-md btn-info'>contact me</button>
+            <button className='btn btn-md btn-info'>Resume</button>
+          </div>
+        </p>
+      </div>
+
+      <div className='services'>
+        <div className="container">
+          <div className="row">
+
+            <div className="col-12 col-md-3">
+              <div className="text-center card p-3">
+                <h3 className="">Quick Delivery</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3">
+              <div className="text-center card p-3">
+                <h3 className="">Quick Delivery</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3">
+              <div className="text-center card p-3">
+                <h3 className="">Quick Delivery</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-3">
+              <div className="text-center card p-3">
+                <h3 className="">Quick Delivery</h3>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="language">
+        <div className="container">
+          <div className="row align-items-center">
+
+            <div className="col-12 col-md-6">
+              <img src="./../../language.svg" alt="" />
+            </div>
+
+            <div className="col-12 col-md-6">
+              
+              <div className='mb-5'>
+              <AiFillHtml5 size={40}/>
+              <FaCss3Alt size={40}/>
+              <TbBrandJavascript size={40}/>
+              <BsBootstrap size={40}/>
+              </div>
+
+              {
+                info?.map((data) =>(
+                  <div className="d-flex align-items-center">
+                    {data?.icon}
+                    <p>{data?.text}</p>
+                  </div>
+                ))
+              }
+
+              
+
+              
+
+            </div>
+
+          </div>
+        </div>
+
       </div>
     
     </div>
